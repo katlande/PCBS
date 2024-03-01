0cd71d9e6c49b933f55d9c74627d964306e095bc
Kathryn Lande <46092714+katlande@users.noreply.github.com>
2024-03-01 11:03:59 -0800

    Add files via upload

--- R/tilt.R
+++ R/tilt.R
@@ -1,3 +1,3 @@
 tilt <- function(){
-  ggplot2::theme(axis.text.x = element_text(colour = "black", angle=45, vjust=1, hjust=1))
+  ggplot2::theme(axis.text.x = ggplot2::element_text(colour = "black", angle=45, vjust=1, hjust=1))
 }
