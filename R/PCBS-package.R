aa1c8fc4420d536d7458fc9f82b3eb6b6d8f3fd5
Kathryn Lande <46092714+katlande@users.noreply.github.com>
2024-04-17 08:46:55 -0700

    fix for CRAN note

---
+++ R/PCBS-package.R
@@ -0,0 +1,6 @@
+#' @keywords internal
+"_PACKAGE"
+utils::globalVariables(c(".", "ID", "PC1", "PC2", "PC_Score", "Set", "abs.order", ".", "best", "bin", "meanScore", "n.seeds", "pos", "se_max", "se_min", "x", "y"))
+## usethis namespace: start
+## usethis namespace: end
+NULL
