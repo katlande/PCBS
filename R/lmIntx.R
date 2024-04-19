0305cce91ad9c42d1996b3339a5af1b874183863
Kathryn Lande <46092714+katlande@users.noreply.github.com>
2024-04-19 09:45:22 -0700

    Update lmIntx.R

--- R/lmIntx.R
+++ R/lmIntx.R
@@ -3,8 +3,8 @@ lmIntx <- function(fit1, fit2, rnd=2) {
   m1<- fit1$coefficient[2]  #slope for fit1
   b2<- fit2$coefficient[1]  #y-int for fit2
   m2<- fit2$coefficient[2]  #slope for fit2
-  if(m1==m2 & b1==b2) {print("Lines are identical")
-  } else if(m1==m2 & b1 != b2) {print("Lines are parallel")
+  if(m1==m2 & b1==b2) {warning("Lines are identical")
+  } else if(m1==m2 & b1 != b2) {warning("Lines are parallel")
   } else {
     x <- (b2-b1)/(m1-m2)      #solved general equation for x
     y <- m1*x + b1            #plug in the result
