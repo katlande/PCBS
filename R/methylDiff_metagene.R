8471be9a1bc2c89209780fa851ec6a013dfeb6a6
Kathryn Lande <46092714+katlande@users.noreply.github.com>
2024-08-01 15:00:35 -0700

    Update methylDiff_metagene.R

--- R/methylDiff_metagene.R
+++ R/methylDiff_metagene.R
@@ -35,7 +35,7 @@ methylDiff_metagene <- function(chromDictMethObj=NULL, regions, bin=100, title="
       ggplot2::geom_smooth(colour=linecol, se=F, formula = y ~ x, method = "loess")+
       Ol_Reliable()+ ggplot2::xlab(xaxis)+ ggplot2::ylab(paste0(yaxis, "\n"))+ ggplot2::ggtitle(title)+
       ggplot2::scale_x_continuous(expand=c(0,0))+
-      ggplot2::scale_y_continuous(expand=c(0,0), limits=c(-1,1) -> g
+      ggplot2::scale_y_continuous(expand=c(0,0), limits=c(-1,1)) -> g
     
     return(g)
   } else {
