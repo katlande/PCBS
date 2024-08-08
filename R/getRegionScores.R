747168bc3d4ea248c411f4ccc49c9669bfeeff30
Kathryn Lande <46092714+katlande@users.noreply.github.com>
2024-08-08 16:22:40 -0700

    Update getRegionScores.R

--- R/getRegionScores.R
+++ R/getRegionScores.R
@@ -1,46 +1,48 @@
 #' @export
-getRegionScores <- function(ranks, regions){
-
-  if(! "abs.order" %in% colnames(ranks)){
-    message("No ranks in input file; adding ranks. Save computational time by running addRanks() on your input file if you plan on running getRegionScores() more than once.")
-    addRanks(ranks)->ranks
-  }
-
-  unlist(lapply(1:1000, function(x){
-    ranks$PC_Score[sample(1:nrow(ranks), 100)]
-  })) -> n
-  upop <- mean(n)
-  sdpop <- sd(n)
-
-  apply(regions, 1, FUN=function(x){
-    paste0(x[1],":",as.numeric(x[2]):as.numeric(x[3]))
-  }) -> r
-
-  ranks[row.names(ranks)%in%unique(unlist(r)),]->tmp
-  names(r)<-regions[[4]]
-
-  means <- c()
-  nCpGs <- c()
-  zv <- c()
-  pv <- c()
-  for(i in 1:length(r)){
-    a<-tmp$PC_Score[row.names(tmp)%in%r[[i]]]
-    means <- c(means,mean(a))
-    nCpGs <- c(nCpGs, length(a))
-
-    zv <- c(zv,((mean(a)-upop)/sdpop))
-    if(length(a) > 3){
-      if(length(a) < length(n)){
-        b <- n[sample(1:length(n), length(a))]
-      } else{
-        b <- n
-      }
-      pv <- c(pv, t.test(a, b)$p.value)
-    } else{
-      pv <- c(pv, NA)
+getRegionScores <- function(ranks=NULL, regions, chromDictObj=NULL){
+  
+  if(is.null(chromDictObj) & is.null(ranks)){
+    warning("Both ranks and chromDictObj are NULL. At least one must be used.")
+  } else{
+    if(is.null(chromDictObj)){
+      message("Creating chromDict Object. If you plan to run the getRegionScores() function multiple times, it is strongly recommended to generate a chromDict Object with the chromDict() function, and specify it with getRegionScores(..., chromDictObj=OBJECT). This is the most computationally intensive part of most PCBS functions, and only needs to be done once per dataset.")
+      chromDict(ranks) -> chromDictObj
     }
-
+    
+    # background distribution for each chromosome:
+    message("Getting Background Sites...")
+    local_list <- list()
+    for(i in names(chromDictObj)){
+      d <- chromDictObj[[i]]
+      v <- ifelse(nrow(d)*0.01 > 10000, 10000, ceiling(nrow(d)*0.01))
+      v <- ifelse(v < 100, 100, v)
+      n <- d$PC_Score[sample(1:nrow(d),v)]
+      local_list <- append(local_list, list(n))
+    }
+    names(local_list) <- names(chromDictObj)
+    
+    message(paste("Checking significance at", nrow(regions), "sites..."))
+    means <- c()
+    nCpGs <- c()
+    zv <- c()
+    pv <- c()
+    for(i in 1:nrow(regions)){
+      df <- chromDictObj[[regions[[1]][i]]]
+      df.main <- na.omit(df[ .( c((regions[[2]][i]:regions[[3]][i])) ) ])
+      
+      u <- mean(df.main$PC_Score)
+      n <- nrow(df.main)
+      means <- c(means,u)
+      nCpGs <- c(nCpGs, n)
+      zv <- c(zv,((u-mean(local_list[[regions[[1]][i]]]))/sd(local_list[[regions[[1]][i]]])))
+      
+      if(length(local_list[[regions[[1]][i]]]) > 3 & length(df.main$PC_Score) > 3){
+        pv <- c(pv, t.test(df.main$PC_Score, local_list[[regions[[1]][i]]])$p.value)
+      } else{
+        pv <- c(pv, NA)
+      }
+      }
+    return(data.frame(chrom=regions[[1]], start=regions[[2]], end=regions[[3]], 
+                      feature=regions[[4]], meanPC=means, nCpG=nCpGs, Z=zv, p=pv))
   }
-
-  return(data.frame(feature=names(r), meanPC=means, nCpG=nCpGs, Z=zv, p=pv))
 }
